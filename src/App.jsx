71041ed49376575396b37b49f9941763aeabbb13
Marcel Ramos <marceldavid@hotmail.com>
2025-11-19 19:02:59 -0500

    set React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+/** Function where we define our routes (Imperative way to create Router).
+ * Imper. way is necessary in order to enable data fetching/ data loading with
+ * react roater.
+ * We can use powerful APi's like data loaders, data actions or data fetchers.
+ * We do that by passing an array of object where each obj is a route
+ */
+const router = createBrowserRouter([
+  {
+    path: "/", //the root
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "/order/:orderId", //with params
+    element: <Order />,
+  },
+]);
+
 function App() {
-  const x = 23;
-  return <div>Helo Vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
