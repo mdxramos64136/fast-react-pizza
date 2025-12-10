c7890fa58e7402fef86c832cbdf59ff4bd461f7d
Marcel Ramos <marceldavid@hotmail.com>
2025-12-10 18:16:21 -0500

    Apply style IV (font)

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,6 @@ export default defineConfig([
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-        "plugin:prettier/recommended"
     ],
     languageOptions: {
       ecmaVersion: 2020,
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
 import Username from "../features/user/Username";
 
-
 function Header() {
   return (
-    <header className=" text-xl flex items-center justify-between bg-yellow-500 px-4 py-3 sm:px-6  border-b border-stone-200 ">
-      <Link to="/" className="tracking-widest">Fast ReactPizza Co.</Link>
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 font-mono text-xl sm:px-6">
+      <Link to="/" className="tracking-widest">
+        Fast ReactPizza Co.
+      </Link>
       <SearchOrder />
-      <Username/>
+      <Username />
     </header>
   );
 }
