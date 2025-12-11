e4cfbc4a51a460acd86dda4110ee42b71af4be5b
Marcel Ramos <marceldavid@hotmail.com>
2025-12-10 19:11:56 -0500

    Apply style V (menu)

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,25 +1,25 @@
-import { Link } from 'react-router-dom';
-import LinkButton from '../../ui/LinkButton';
-import Button from '../../ui/Button';
+import { Link } from "react-router-dom";
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -36,7 +36,9 @@ function Cart() {
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Button to="/order/new">Order pizzas</Button>
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
         <button>Clear cart</button>
       </div>
     </div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-function Button({children, disabled,to}) {
-    const className = `bg-yellow-400 uppercase font-semibold 
-             text-stone-800 py-3 px-4 inline-block tracking-wide 
+function Button({ children, disabled, to, type }) {
+  //based on the type, get the type from the object
+  const base = `bg-yellow-400 uppercase font-semibold 
+             text-stone-800 inline-block tracking-wide 
              rounded-full hover:bg-yellow-500
              hover:text-yellow-100 transition-colors 
              duration-500 focus:ring-yellow-500 
              focus:bg-yellow-300 focus:outline-none focus:ring 
-             focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4`
-    
-    //if there is 'to' prop, return a <Link> not a Button
-    if(to) return <Link className={className} to={to}>{children}</Link>
+             focus:ring-offset-2 disabled:cursor-not-allowed `;
+
+  const styles = {
+    primary: base + " md:px-6 md:py-4 py-3 px-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+  };
+
+  //if there is 'to' prop, return a <Link> not a Button
+  if (to)
     return (
-        <button disabled={disabled} 
-             className={className}>
-            {children}
-        </button>
-    )
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+  return <button disabled={disabled}>{children}</button>;
 }
 
-export default Button
+export default Button;
